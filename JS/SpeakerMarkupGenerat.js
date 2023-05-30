60938ad3709890b5cdce19bb62498414d3dcaca6
Melkamu Almawu Ayen <malmawu@gmail.com>
2023-05-30 10:37:35 +0300

    update README.md

--- JS/SpeakerMarkupGenerat.js
+++ JS/SpeakerMarkupGenerat.js
@@ -1,51 +1,63 @@
 const SpeakerInformation = [
   {
-    id: '01',
-    SpeakerImage: 'Asset/speaker_01.svg',
-    SpeakerAlt: 'Here is speaker image',
-    SpeakerName: 'Yochai Benkler',
-    SpeakerTitle: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-    SpeakerProfile: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
+    id: "01",
+    SpeakerImage: "Asset/speaker_01.svg",
+    SpeakerAlt: "Here is speaker image",
+    SpeakerName: "Yochai Benkler",
+    SpeakerTitle:
+      "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
+    SpeakerProfile:
+      "Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006",
   },
   {
-    id: '02',
-    SpeakerImage: '/Asset/speaker_03.png',
-    SpeakerAlt: 'Here is speaker image',
-    SpeakerName: 'Yochai Benkler',
-    SpeakerTitle: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-    SpeakerProfile: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
+    id: "02",
+    SpeakerImage: "Asset/speaker_03.png",
+    SpeakerAlt: "Here is speaker image",
+    SpeakerName: "Yochai Benkler",
+    SpeakerTitle:
+      "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
+    SpeakerProfile:
+      "Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006",
   },
   {
-    id: '03',
-    SpeakerImage: '/Asset/speaker_05.png',
-    SpeakerAlt: 'Here is speaker image',
-    SpeakerName: 'Yochai Benkler',
-    SpeakerTitle: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-    SpeakerProfile: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
+    id: "03",
+    SpeakerImage: "Asset/speaker_05.png",
+    SpeakerAlt: "Here is speaker image",
+    SpeakerName: "Yochai Benkler",
+    SpeakerTitle:
+      "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
+    SpeakerProfile:
+      "Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006",
   },
   {
-    id: '04',
-    SpeakerImage: '/Asset/speaker_06.png',
-    SpeakerAlt: 'Here is speaker image',
-    SpeakerName: 'Yochai Benkler',
-    SpeakerTitle: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-    SpeakerProfile: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
+    id: "04",
+    SpeakerImage: "Asset/speaker_06.png",
+    SpeakerAlt: "Here is speaker image",
+    SpeakerName: "Yochai Benkler",
+    SpeakerTitle:
+      "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
+    SpeakerProfile:
+      "Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006",
   },
   {
-    id: '04',
-    SpeakerImage: '/Asset/speaker_01 1.svg',
-    SpeakerAlt: 'Here is speaker image',
-    SpeakerName: 'Yochai Benkler',
-    SpeakerTitle: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-    SpeakerProfile: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
+    id: "05",
+    SpeakerImage: "Asset/speaker_011.svg",
+    SpeakerAlt: "Here is speaker image",
+    SpeakerName: "Yochai Benkler",
+    SpeakerTitle:
+      "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
+    SpeakerProfile:
+      "Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006",
   },
   {
-    id: '04',
-    SpeakerImage: '/Asset/speaker_01.svg',
-    SpeakerAlt: 'Here is speaker image',
-    SpeakerName: 'Yochai Benkler',
-    SpeakerTitle: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-    SpeakerProfile: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
+    id: "06",
+    SpeakerImage: "Asset/speaker_01.svg",
+    SpeakerAlt: "Here is speaker image",
+    SpeakerName: "Yochai Benkler",
+    SpeakerTitle:
+      "Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School",
+    SpeakerProfile:
+      "Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006",
   },
 ];
 
@@ -90,7 +102,7 @@ const generateSingleSpeakersMarkup = (SpeakerInformation) => {
 
 const generateAllSpeakersMarkup = (shouldBeExpanded) => {
   let speakers;
-  let btn = '';
+  let btn = "";
 
   if (shouldBeExpanded) {
     speakers = [...SpeakerInformation];
@@ -108,42 +120,45 @@ const generateAllSpeakersMarkup = (shouldBeExpanded) => {
     }
   }
 
-  const listItems = speakers.map((speaker) => generateSingleSpeakersMarkup(speaker));
+  const listItems = speakers.map((speaker) =>
+    generateSingleSpeakersMarkup(speaker)
+  );
 
   // Return the speakers markup with the button
   return `
       <div class="speakers-section">
-        ${listItems.join('\n')}
+        ${listItems.join("\n")}
       </div>
       ${btn}
     `;
 };
 
-const speakersTarget = document.querySelector('.Speaker-content');
+const speakersTarget = document.querySelector(".Speaker-content");
 
 const mountSpeakersList = () => {
   const isMobile = window.innerWidth < 768;
-  const shouldBeExpanded = !isMobile || speakersTarget.getAttribute('data-expanded') === 'false';
+  const shouldBeExpanded =
+    !isMobile || speakersTarget.getAttribute("data-expanded") === "false";
 
-  speakersTarget.setAttribute('data-expanded', shouldBeExpanded);
+  speakersTarget.setAttribute("data-expanded", shouldBeExpanded);
   speakersTarget.innerHTML = generateAllSpeakersMarkup(shouldBeExpanded);
 };
 
 const handleButtonClick = (e) => {
-  if (e.target.id === 'see-more-speakers-btn') {
-    speakersTarget.setAttribute('data-expanded', true);
+  if (e.target.id === "see-more-speakers-btn") {
+    speakersTarget.setAttribute("data-expanded", true);
     speakersTarget.innerHTML = generateAllSpeakersMarkup(true);
-  } else if (e.target.id === 'see-fewer-speakers-btn') {
-    speakersTarget.setAttribute('data-expanded', false);
+  } else if (e.target.id === "see-fewer-speakers-btn") {
+    speakersTarget.setAttribute("data-expanded", false);
     speakersTarget.innerHTML = generateAllSpeakersMarkup(false);
   }
 };
 
 // Add event listener to the "MORE"/"LESS" buttons
-speakersTarget.addEventListener('click', handleButtonClick);
+speakersTarget.addEventListener("click", handleButtonClick);
 
 // Add event listener to the window's resize event
-window.addEventListener('resize', mountSpeakersList);
+window.addEventListener("resize", mountSpeakersList);
 
 // Mount the speakers' list initially
 mountSpeakersList();
